661d208ddb190ed813e302d7fa3cfee1e32e6bd7
HoangTran <trannhathoang231@gmail.com>
2022-04-15 04:56:23 +0700

    first commit

---
+++ js/baitap1.js
@@ -0,0 +1,28 @@
+/**
+ * Khoi 1:Input
+ * luong1Ngay
+ * soNgayLam
+ * Khoi 2:
+ * B1:tạo biến và gán giá trị
+ * 
+ * B2:xây dựng công thức
+ * tongLuong = luong1Ngay * soNgayLam
+ * 
+ * B3:hiển thị kết quả
+ * 
+ * Khoi 3:Output
+ * +tongLuong
+ * 
+ */
+
+// B1
+var luong1Ngay = 100000;
+var soNgayLam = 5;
+var tongLuong = 0;
+
+// B2:
+tongLuong = luong1Ngay * soNgayLam
+
+//B3:
+// toLocaleString: hiển thị dấu chia phần ngàn cho number
+console.log("Tổng lương: " + tongLuong.toLocaleString() + " VNĐ")
---
+++ js/baitap2.js
@@ -0,0 +1,25 @@
+/**
+ * Khoi 1:input
+ * var num1... num5
+ * 
+ * Khoi 2
+ * tb = (num1 + num2 + num3 +num4 +num5) / 5;
+ * 
+ * Khoi 3
+ * + tb
+ */
+
+var num1 = 5.5;
+var num2 = 10.435;
+var num3 = 4.345;
+var num4 = 6.765;
+var num5 = 7.345;
+var tb = 0;
+
+tb = (num1 + num2 + num3 +num4 +num5) / 5;
+
+console.log("So trung binh : " + tb)
+
+// console.log("Làm tròn " + Math.round(tb))
+
+// console.log("Làm tròn, lấy bao nhiêu số sau dấu . " + tb.toFixed(1))
